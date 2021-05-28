4352b99c9b56451ec183bb816b0d1f855e420f86
Ehsan Ullah Khan <ullahkhanihsan@yahoo.com>
2021-05-28 10:41:37 +0500

    updated schemas

--- backend/web/schemas.py
+++ backend/web/schemas.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 class VMI(BaseModel):
@@ -9,7 +11,13 @@ class VMI(BaseModel):
 class Command_schema(BaseModel):
     c_id: int
     command: str
-    result: str
+    hostname : str
+    username : str
+    port : int
+    task_id : str
+    result : Optional[str] = None
+    task_state : str
+    exception : Optional[str] = None
 
     class Config:
         orm_mode = True
