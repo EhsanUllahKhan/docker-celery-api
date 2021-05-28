374b41bdca502ddc24db92568d06689c9d789367
Ehsan Ullah Khan <ullahkhanihsan@yahoo.com>
2021-05-28 10:18:01 +0500

    updated model definition for new task requirements

--- backend/models/models.py
+++ backend/models/models.py
@@ -7,5 +7,11 @@ class Command(Base):
     __tablename__ = "command"
 
     c_id = Column(Integer, primary_key=True, index=True)
-    command = Column(String(length=50))
-    result = Column(String(length=5000))
+    hostname = Column(String(length=30))
+    username = Column(String(length=50))
+    port = Column(Integer)
+    command = Column(String)
+    task_id=Column(String)
+    result = Column(String, nullable=True)
+    exception = Column(String, nullable=True)
+
